d4467628bb2bab09df6b3022bbddb4fd2a917d88
1684838553 <1684838553@qq.com>
2024-10-21 15:36:31 +0800

    两个新的生命周期钩子

--- vue-router/src/components/News.vue
+++ vue-router/src/components/News.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="news">
     <ul>
+        <div :style="{ opacity }">这是news组件</div>
         <li>news001 <input type="text" /></li>
         <li>news002 <input type="text" /></li>
         <li>news003 <input type="text" /></li>
@@ -14,7 +15,22 @@ export default {
   name: "News",
   data() {
     return {
+        opacity: 1
     };
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+        console.log('@')
+        this.opacity -= 0.1;
+        if(this.opacity <= 0) this.opacity = 1
+    }, 1000)
+  },
+  activated() {
+    console.log('news被激活')
+  },
+  deactivated() {
+    console.log('news失活')
+    clearInterval(this.timer)
   },
 };
 </script>
