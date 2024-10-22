51a84990a0942dcdad36fb58075276fc12609c31
1684838553 <1684838553@qq.com>
2024-10-22 11:42:06 +0800

    Vue3.0中的响应式原理

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,44 +1,56 @@
 <template>
   <h3>一个人的信息</h3>
-  <h3>姓名: {{ name }}</h3>
-  <h3>年龄: {{ age }}</h3>
-  <h3>工作种类: {{ job.type }}</h3>
-  <h3>工作薪水: {{ job.salary }}</h3>
+  <h3>姓名: {{ person.name }}</h3>
+  <h3>年龄: {{ person.age }}</h3>
+  <h3>工作种类: {{ person.job.type }}</h3>
+  <h3>工作薪水: {{ person.job.salary }}</h3>
+  <h3>性别: {{ person.sex }}</h3>
   <h3>
-    爱好：{{ hobby }}
+    爱好：{{ person.hobbies }}
   </h3>
   <button @click="changeInfo">修改人的信息</button>
+  <button @click="addSex">添加sex属性</button>
+  <button @click="deleteName">删除name属性</button>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { reactive } from 'vue';
 export default {
   name: 'App',
   components: {
   },
   setup() {
-    let name = ref('张三');
-    let age = ref(18);
-    let job = reactive({
-      type: '前端工程师',
-      salary: '30k'
+    const person = reactive({
+      name: '张三',
+      age: 18,
+      job: {
+        type: '前端工程师',
+        salary: '30k'
+      },
+      hobbies: ['打篮球', '学习']
     })
-    let hobby = reactive(['打篮球', '学习']);
+
+    function addSex() {
+      person.sex = '男';
+    }
+
+    function deleteName() {
+      delete person.name;
+    }
 
     function changeInfo() {
-      name.value = '李四';
-      age.value = 23;
-      job.type = 'UI设计师';
-      job.salary = '50k';
-      hobby[2] = '做饭';
+      person.name = '李四';
+      person.age = 23;
+      person.job.type = 'UI设计师';
+      person.job.salary = '50k';
+      person.hobbies[2] = '做饭';
     }
 
     return {
-      name,
-      age,
+      person,
       changeInfo,
-      job,
-      hobby
+      addSex,
+      deleteName
     }
   }
 }
