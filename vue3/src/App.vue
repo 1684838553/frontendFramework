87c22ad39fd1f3744afcc433e0cbc0a18b3035b8
1684838553 <1684838553@qq.com>
2024-10-22 10:19:55 +0800

    ref函数

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,26 +2,36 @@
   <h3>一个人的信息</h3>
   <h3>姓名: {{ name }}</h3>
   <h3>年龄: {{ age }}</h3>
+  <h3>工作种类: {{ job.type }}</h3>
+  <h3>工作薪水: {{ job.salary }}</h3>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   name: 'App',
   components: {
   },
   setup() {
-    let name = '张三';
-    let age = 18;
+    let name = ref('张三');
+    let age = ref(18);
+    let job = ref({
+      type: '前端工程师',
+      salary: '30k'
+    })
 
     function changeInfo() {
-      console.log('changeInfo')
+      name.value = '李四';
+      age.value = 23;
+      job.value.type = 'UI设计师';
     }
 
     return {
       name,
       age,
-      changeInfo
+      changeInfo,
+      job
     }
   }
 }
