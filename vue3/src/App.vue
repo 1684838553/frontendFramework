7f8360b80fc15ad3aa1999939dbe629fdc8a8df9
1684838553 <1684838553@qq.com>
2024-10-22 11:03:01 +0800

    reactive函数

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -4,11 +4,14 @@
   <h3>年龄: {{ age }}</h3>
   <h3>工作种类: {{ job.type }}</h3>
   <h3>工作薪水: {{ job.salary }}</h3>
+  <h3>
+    爱好：{{ hobby }}
+  </h3>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 export default {
   name: 'App',
   components: {
@@ -16,22 +19,26 @@ export default {
   setup() {
     let name = ref('张三');
     let age = ref(18);
-    let job = ref({
+    let job = reactive({
       type: '前端工程师',
       salary: '30k'
     })
+    let hobby = reactive(['打篮球', '学习']);
 
     function changeInfo() {
       name.value = '李四';
       age.value = 23;
-      job.value.type = 'UI设计师';
+      job.type = 'UI设计师';
+      job.salary = '50k';
+      hobby[2] = '做饭';
     }
 
     return {
       name,
       age,
       changeInfo,
-      job
+      job,
+      hobby
     }
   }
 }
