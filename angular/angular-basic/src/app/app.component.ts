9a5b88aac43bbc37f61fd15ac630deb10e9b7b53
1684838553 <1684838553@qq.com>
2025-07-21 10:39:45 +0800

    显示数据和绑定属性

--- angular/angular-basic/src/app/app.component.ts
+++ angular/angular-basic/src/app/app.component.ts
@@ -3,10 +3,30 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,  // 声明为独立模块
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'angular-test';
+  gender = 0;
+  catImg = {
+    url: '/cat.png',
+    alt: 'cat'
+  };
+  dogImg = {
+    url: '/dog.png',
+    alt: 'dog'
+  };
+  customTitle = "自定义文案"
+
+  getValue(): number {
+    return 23;
+  }
+
+  get job(): string {
+    return '战士';
+  }
 }
